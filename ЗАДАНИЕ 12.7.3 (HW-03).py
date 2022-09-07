53d3cfa4be501e84d01ed03f04b2296524354e59
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2022-09-07 15:39:22 +0300

    practical task

---
+++ ЗАДАНИЕ 12.7.3 (HW-03).py
@@ -0,0 +1,10 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+money = int(input("Введите сумму которую планируете положить под проценты: "))
+
+per = list(per_cent.values())
+deposit = list(map(lambda x: x * money/100, per))
+deposit_max = max(deposit)
+
+print(deposit)
+print("Максимальная сумма, которую вы можете заработать", deposit_max)
