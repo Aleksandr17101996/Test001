637dc30738f1f2f42072cfa78732d1acd15760b3
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2022-09-07 17:33:52 +0300

    Задание 12.7.3

--- ЗАДАНИЕ 12.7.3 (HW-03).py
+++ ЗАДАНИЕ 12.7.3 (HW-03).py
@@ -1,6 +1,6 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
-money = int(input("Введите сумму которую планируете положить под проценты: "))
+money = int(input("Введите сумму которую планируете положить под проценты:"))
 
 per = list(per_cent.values())
 deposit = list(map(lambda x: x * money/100, per))
